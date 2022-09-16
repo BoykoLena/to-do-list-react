d3f500c0f501a17f724011f0f9e2bd652395fbfc
BoykoLena <boykolena15@gmail.com>
2022-09-16 14:42:32 +0300

    make main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import List from "./components/List";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>What's the plan for today? </h1>
+      <List />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import "./List.css";
+
+function List() {
+  return (
+    <div className="List">
+      <form>
+        <input type="text" placeholder="What needs to be done ?"></input>
+      </form>
+    </div>
+  );
+}
+
+export default List;
